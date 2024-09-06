6c992fb51faf55b0cc6c2e45f8e58e6c84350f80
Roberto Pio Iannello <s5201538@studenti.unige.it>
2024-09-06 12:21:59 +0200

    better php support

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -30,4 +30,29 @@ return {
     'Exafunction/codeium.vim',
     event = 'BufEnter'
   },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = {
+      sources = {
+        { name = "nvim_lsp", trigger_characters = { "-"}},
+        { name = "luasnip" },
+        { name = "buffer"},
+        { name = "nvim_lua"},
+        { name = "path"},
+      },
+    },
+
+    dependencies = {
+      --snippets plugin
+      "L3MON4D3/LuaSnip",
+      config = function()
+        require("nvchad.configs.luasnip")
+
+        -- estensione di html snippets a php
+        local luasnip = require "luasnip"
+        luasnip.filetype_extend("php", { "html" })
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+  },
 }
