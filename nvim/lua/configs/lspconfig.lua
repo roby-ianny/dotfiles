bb860d2aab4158c9522d07af5e7c3641647e52b5
Roberto Pio Iannello <s5201538@studenti.unige.it>
2024-09-22 16:34:12 +0200

    Default Nvchad

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "cssls", "phpactor" }
+local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -17,30 +17,8 @@ for _, lsp in ipairs(servers) do
 end
 
 -- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
+-- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
---
--- personal configs
-lspconfig.html.setup{
-  filetypes = { "html", "php" },
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
-
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = nvlsp.capabilities,
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "php" },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-})
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,3 @@
-vim.g.lua_snippets_path = vim.fn.stdpath "config" .. "/lua/lua_snippets"
-
 return {
   {
     "stevearc/conform.nvim",
@@ -11,48 +9,17 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "php", "javascript"
-  		},
-  	},
-  },
-  -- PERSONAL CONFIGURATION? 
-  {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter'
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      sources = {
-        { name = "nvim_lsp", trigger_characters = { "-"}},
-        { name = "luasnip" },
-        { name = "buffer"},
-        { name = "nvim_lua"},
-        { name = "path"},
-      },
-    },
-
-    dependencies = {
-      --snippets plugin
-      "L3MON4D3/LuaSnip",
-      config = function()
-        require("nvchad.configs.luasnip")
-
-        -- estensione di html snippets a php
-        local luasnip = require "luasnip"
-        luasnip.filetype_extend("php", { "html" })
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
-  },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
