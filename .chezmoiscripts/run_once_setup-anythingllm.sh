b1e8cd54036827c6e48423f262afec5bf8824040
Roberto Pio Iannello <s5201538@studenti.unige.it>
2026-06-06 10:41:48 +0200

    Started to add vscodium settings and moved AnythingLLM to appimages dir * Modified anythingllm setup script * Added continue.dev vscode extension config * Added vscodium settings.json * Addes vscodium snippets folder

--- .chezmoiscripts/run_once_setup-anythingllm.sh
+++ .chezmoiscripts/run_once_setup-anythingllm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/AppImages # set the installation directory
+
 # Download the installer script to wherever you want to run it from
 curl -fsSL https://cdn.anythingllm.com/latest/installer.sh -o installer.sh
  
@@ -7,4 +9,4 @@ curl -fsSL https://cdn.anythingllm.com/latest/installer.sh -o installer.sh
 chmod +x installer.sh
  
 # Run the script
-./installer.sh
+ANYTHING_LLM_INSTALL_DIR="~/AppImages/" ./installer.sh
