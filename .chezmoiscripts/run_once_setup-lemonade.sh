9f87cd0f976105ae986fcc93ea1ba1e8f187a468
Roberto Pio Iannello <s5201538@studenti.unige.it>
2026-07-01 21:52:19 +0200

    Removed lemonade and switch to vscode rpm package

--- .chezmoiscripts/run_once_setup-lemonade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/lemonade-sdk/lemonade/releases/latest/download/lemonade-server-10.7.0-fc44.x86_64.rpm
-sudo dnf install ./lemonade-server-10.7.0-fc44.x86_64.rpm
-rm lemonade-server-10.6.0-fc44.x86_64.rpm 
-
-sudo systemctl enable lemond.service
-sudo systemctl start lemond.service
